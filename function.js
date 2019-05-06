6ce4d9eb02cbcd44b8995a12c60debecc95217c3
rifqicode <muhammadrifqi.tb@gmail.com>
2019-05-06 14:53:03 +0700

    update

--- function.js
+++ function.js
@@ -1,12 +1,3 @@
-  // my script
-  function validate_page() {
-    let value = parseInt(document.getElementById('page').value)
-
-    if (value > 3) {
-      document.getElementById('page').value = 3
-    }
-  }
-
   function validate_row() {
     let value = parseInt(document.getElementById('row').value)
 
@@ -16,17 +7,17 @@
   }
 
   function scrapping() {
-    let page = parseInt(document.getElementById('page').value),
-        row = parseInt(document.getElementById('row').value),
+
+    let row = parseInt(document.getElementById('row').value),
         table = document.getElementById('table').getElementsByTagName('tbody')[0];
 
-    if (page > 0 && row > 0) {
+    if (row > 0) {
 
       var {PythonShell} = require("python-shell"),
           path = require("path"),
           options = {
             scriptPath : path.join(__dirname, '/./core/'),
-            args : [page , row]
+            args : [row]
           }
 
       PythonShell.run('core.py', options, function (err, results) {
@@ -41,14 +32,12 @@
             let parse = value.split('--');
 
             let index = table.rows.length + 1
-            // Create an empty <tr> element and add it to the 1st position of the table:
             var row = table.insertRow(0);
-            // Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
+
             var cell1 = row.insertCell(0);
             var cell2 = row.insertCell(1);
             var cell3 = row.insertCell(2);
 
-            // Add some text to the new cells:
             cell1.innerHTML = no;
             cell2.innerHTML = parse[0];
             cell3.innerHTML = parse[1];
